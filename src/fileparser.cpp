ca028dd35ff9002f167b064760a1bde0b1ba9862
AlexTeos <alex.teos@yandex.ru>
2021-11-11 23:43:51 +0300

    Fix FileParser bugs. Expand FileParser UT coverage.

--- src/fileparser.cpp
+++ src/fileparser.cpp
@@ -4,37 +4,41 @@
 #include <iostream>
 #include <QMap>
 
-bool FileParser::loadFile(const QString &fileName)
+bool FileParser::loadFile(const QString &fileName, const int readLineSize)
 {
     QFile file(fileName.last(fileName.length()-8));
-    QMap<QString, uint32_t> words;
     if(file.exists())
     {
         file.open(QIODevice::ReadOnly);
-        if(file.isOpen()){
-
-            char data[16];
+        if(file.isOpen())
+        {
+            char data[readLineSize];
             QString line;
-            while(file.read(data, 16)){
-                line += QString::fromLocal8Bit(data);
+            int read_cont = file.read(data, readLineSize);
+            while(read_cont)
+            {
+                line += QString::fromLocal8Bit(data,read_cont);
 
-                int space = line.indexOf(" ");
+                int space = line.indexOf(QRegularExpression("[,. \n\r]"));
                 while(space != -1)
                 {
                     QString word = line.first(space).toLower();
                     word.remove(QRegularExpression("[^a-z\\-]"));
-                    words.contains(word) ? words.insert(word, words.value(word)+1) : words.insert(word, 1);
+                    if(word.size())
+                        words.contains(word) ? words.insert(word, words.value(word)+1) : words.insert(word, 1);
 
                     line.remove(0, space + 1);
-                    space = line.indexOf(" ");
+                    space = line.indexOf(QRegularExpression("[,. \n\r]"));
                 }
+
+                read_cont = file.read(data, readLineSize);
             }
             if(line.length())
             {
                 line.remove(QRegularExpression("[^a-z\\-]"));
-                words.contains(line) ? words.insert(line, words.value(line)+1) : words.insert(line, 0);
+                if(line.size())
+                    words.contains(line) ? words.insert(line, words.value(line)+1) : words.insert(line, 0);
             }
-            std::cout << "Done" << std::endl;
             return true;
         }
         else
@@ -44,5 +48,15 @@ bool FileParser::loadFile(const QString &fileName)
         std::cout << "File doesn't exits" << std::endl;
 
     return false;
+}
+
+const QMap<QString, uint32_t>& FileParser::getWords()
+{
+    return words;
+}
+
+void FileParser::reset()
+{
+    words.clear();
 }
 
--- src/fileparser.h
+++ src/fileparser.h
@@ -8,7 +8,14 @@ class FileParser : public QObject
 {
     Q_OBJECT
 public slots:
-    bool loadFile(const QString &fileName);
+    bool loadFile(const QString &fileName, const int readLineSize = 256);
+
+public:
+    const QMap<QString, uint32_t>& getWords();
+    void reset();
+
+private:
+    QMap<QString, uint32_t> words;
 };
 
 #endif // MYCLASS_H
